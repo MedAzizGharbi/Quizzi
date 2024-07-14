c10683d50cd06312c22a40377848ca187a1e4f31
Aziz Gharbi <gharbi.aziz03@gmail.com>
2024-07-14 18:42:54 +0100

    Auth REMOVAL

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import "./App.css";
-import Login from "./Components/Login";
-import Signup from "./Components/Signup";
-import { AuthProvider } from "./Context/AuthContext";
 import Quizz from "./Pages/Quizz";
 import Result from "./Pages/Result";
 import Start from "./Pages/Start";
@@ -9,17 +6,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <AuthProvider>
         <Router>
           <Routes>
             <Route path="/" element={<Start />}></Route>
             <Route path="/quizz" element={<Quizz />}></Route>
             <Route path="/result" element={<Result />}></Route>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
           </Routes>
         </Router>
-      </AuthProvider>
     </>
   );
 }
